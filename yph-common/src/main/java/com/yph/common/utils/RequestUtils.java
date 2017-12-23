04e433d5caaccd8c87c82c06288f7b943b1a7bd6
Hokok <2016271115@qq.com>
2017-12-23 13:37:09 +0800

    添加请求日志输出

---
+++ yph-common/src/main/java/com/yph/common/utils/RequestUtils.java
@@ -0,0 +1,13 @@
+package com.yph.common.utils;
+
+/**
+ * Request 请求工具类，获取request中请求参数等等。
+ * @author : Administrator Hzhan
+ * @create ：2017/12/23
+ **/
+public class RequestUtils {
+
+
+
+
+}
---
+++ yph-common/src/main/java/com/yph/common/utils/SpringContentUtils.java
@@ -0,0 +1,15 @@
+package com.yph.common.utils;
+
+/**
+ * Spring 工具类
+ *
+ * @author : Administrator Hzhan
+ * @create ：2017/12/23
+ **/
+public class SpringContentUtils {
+
+
+
+
+
+}
