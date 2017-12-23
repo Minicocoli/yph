5ef0f46db1bd3b8ea83420e98994a8c2d1f6f9d6
Hokok <2016271115@qq.com>
2017-12-23 10:28:40 +0800

    换成fastJson

---
+++ yph-admin-web/src/main/java/com/yph/controller/index/IndexController.java
@@ -0,0 +1,28 @@
+package com.yph.controller.index;
+
+import org.hibernate.validator.constraints.pl.REGON;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 首页跳转控制器
+ *
+ * @author : Administrator Hzhan
+ * @create ：2017/12/23
+ **/
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello(HttpServletRequest request){
+        return "index";
+    }
+
+}
---
+++ yph-common/src/main/java/com/yph/common/utils/DateUtils.java
@@ -0,0 +1,15 @@
+package com.yph.common.utils;
+
+/**
+ * 时间日期工具类
+ *
+ * @author : Administrator Hzhan
+ * @create ：2017/12/23
+ **/
+public class DateUtils {
+
+
+
+
+
+}
